1331436d5142167dd96795c02fa43effa3d5160d
Dmitry Evteev <haxrelt@gmail.com>
2025-02-18 20:21:14 +0300

    feat: add docs

--- server/cmd/app/main.go
+++ server/cmd/app/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"chat-ppi-server/pkg/services"
+	"fmt"
+)
+
+// main Точка входа
 func main() {
+	authsvc := services.NewAuthService()
+
+	err := authsvc.Auth("admin", "admin")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Авторизация успешна")
+
+	authsvc.Register("pi-21a", "donntu")
 }
